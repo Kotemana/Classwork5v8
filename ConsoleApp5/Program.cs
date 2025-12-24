84a208ca6b12b51634aba1f8d2179c80a3f9c9e3
Leonid Radkevich <laradkevich@gmail.com>
2025-12-24 21:14:29 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿Cat[] cats = new Cat[3];
+cats[0] = new Cat("Whiskers", 2);
+cats[1] = new Cat("Mittens", 4);
+cats[2] = new Cat("Shadow", 3);
+foreach (var cat in cats)
+{
+    Console.WriteLine($"Cat Name: {cat.Name}, Age: {cat.Age}");
+    cat.Meow();
+}
+
+
+
+
+public class Cat
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Cat() { }
+    public Cat(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public void Meow()
+    {
+        Console.WriteLine($"{Name} says Meow!");
+    }
+}
+
+
+public class Flat
+{
+    public int FlatNumber { get; set; }
+    public ConsoleColor Color { get; set; }
+}
+
